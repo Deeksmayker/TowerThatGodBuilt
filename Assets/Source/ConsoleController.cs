7d3b0600103fbd2b8bc004c7279e72348140809f
Alexandra <sasha.dobryakova@gmail.com>
2024-01-29 19:35:41 +0500

    changes

--- Assets/Source/ConsoleController.cs
+++ Assets/Source/ConsoleController.cs
@@ -9,4 +9,27 @@ public class ConsoleController : MonoBehaviour
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private ScrollView historyScrollView;
     // prefab
+
+    [SerializeField] private List<string> commands;
+
+
+    public void OnInputFieldEndEdit()
+    {
+        if (inputField != null)
+        {
+            string fieldResult = inputField.text;
+
+            switch (fieldResult)
+            {
+
+            }
+        }
+    }
+
+
+    public void ShowAllCommands()
+    {
+
+    }
+
 }
