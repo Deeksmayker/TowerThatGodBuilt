51fcba76ec87a393bad2c8ed1f21c9e3d1680aa6
Alexa <sasha.dobryakova@gmail.com>
2024-01-30 00:25:15 +0500

    добавила кнопки для взаимодействия с консолью

--- Assets/Source/ConsoleController.cs
+++ Assets/Source/ConsoleController.cs
@@ -10,11 +10,34 @@ public class ConsoleController : MonoBehaviour
     [SerializeField] private ScrollView historyScrollView;
     // prefab
 
+    [SerializeField] private GameObject consoleWindow;
     [SerializeField] private List<string> commands;
 
+    private bool _isConsoleOpened = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            Debug.Log("SwitchConsoleWindow");
+            SwitchConsoleWindow();
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) /*&& _isConsoleOpened*/)
+        {
+            Debug.Log("Enter");
+        }
+    }
+
+    public void SwitchConsoleWindow()
+    {
+        consoleWindow.SetActive(!_isConsoleOpened);
+        _isConsoleOpened = !_isConsoleOpened;
+    }
 
     public void OnInputFieldEndEdit()
     {
+
         if (inputField != null)
         {
             string fieldResult = inputField.text;
