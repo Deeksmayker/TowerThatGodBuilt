58384e66b2375ca2024d214fd46c63d661089996
Deeksmayker <Lokkins334@gmail.com>
2024-02-11 01:06:39 +0500

    Уворот поворот

--- Assets/Source/Utils/Utils.cs
+++ Assets/Source/Utils/Utils.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.Mathf;
 
 public static class Utils{
     public static void ToggleCursor(bool canISeeYou){
@@ -16,5 +17,14 @@ public static class Utils{
     
     public static Transform GetCameraTransform(){
         return Camera.main.transform;
+    }
+    
+    //Ease functions
+    public static float EaseInOutQuad(float x){
+        return x < 0.5 ? 2 * x * x : 1 - Pow(-2 * x + 2, 2) / 2;
+    }
+    
+    public static float EaseOutQuint(float x){
+        return 1 - Pow(1 - x, 5);
     }
 }
