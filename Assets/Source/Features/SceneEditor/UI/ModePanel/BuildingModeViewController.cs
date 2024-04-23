732d9e487720b32bfde8710102f9659efcefd5b9
Elllias <ellias_vo312@mail.ru>
2024-04-23 09:21:30 +0500

    refactor: refactoring instrument\mode views

---
+++ Assets/Source/Features/SceneEditor/UI/ModePanel/BuildingModeViewController.cs
@@ -0,0 +1,23 @@
+﻿using Source.Features.SceneEditor.Enums;
+using Source.Features.SceneEditor.Interfaces;
+
+namespace Source.Features.SceneEditor.UI.ModePanel
+{
+    public class BuildingModeViewController : IChangeStateListener<EBuildingState>
+    {
+        private const string CURRENT_MODE_TEXT_FORMAT = "Current Mode: {0}";
+        
+        private readonly TextView _textView;
+
+        public BuildingModeViewController(TextView textView)
+        {
+            _textView = textView;
+            _textView.SetText(string.Format(CURRENT_MODE_TEXT_FORMAT, EBuildingState.Disabled));
+        }
+        
+        public void OnStateChange(EBuildingState state)
+        {
+            _textView.SetText(string.Format(CURRENT_MODE_TEXT_FORMAT, state.ToString()));
+        }
+    }
+}
---
+++ Assets/Source/Features/SceneEditor/UI/ModePanel/TextView.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Source.Features.SceneEditor.UI.ModePanel
+{
+    public class TextView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+        
+        public void SetText(string text)
+        {
+            _text.text = text;
+        }
+    }
+}
