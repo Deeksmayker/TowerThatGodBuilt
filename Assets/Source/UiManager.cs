bd66920870c501e03182927c42fe7691084bdee0
Katarsis <lokkins334@gmail.com>
2024-07-25 06:21:11 +0500

    спринт картинки

--- Assets/Source/UiManager.cs
+++ Assets/Source/UiManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,4 +14,28 @@ public class UiManager : MonoBehaviour{
     }
     
     public Image speedVignette;
+    
+    public Sprite sprintFillSprite;
+       
+    public Image[] sprintChargesImages;
+    [NonSerialized] public Image[] sprintFillImages;
+    // public Sprite sprintSprite;
+    // public Sprite[] srpintChargeSprites;
+    
+    private void Start(){
+        sprintFillImages = new Image[sprintChargesImages.Length];
+        for (int i = 0; i < sprintChargesImages.Length; i++){
+            var obj = new GameObject("FillImage");
+            obj.transform.parent = sprintChargesImages[i].transform;
+            obj.transform.localScale *= 2;
+            Image img = obj.AddComponent<Image>();
+            img.transform.localPosition = Vector3.zero;
+            img.sprite = sprintFillSprite;
+            img.type = Image.Type.Filled;
+            img.fillMethod = Image.FillMethod.Horizontal;
+            img.fillOrigin = (int)Image.OriginHorizontal.Right;
+            img.fillAmount = (float)i / (float)sprintChargesImages.Length;
+            sprintFillImages[i] = img;
+        }
+    }
 }
